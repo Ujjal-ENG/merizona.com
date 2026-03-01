5d27b64baa9d001614f8ccf4ad82217e9b102d8c
ujjal <ujjalroy7862@gmail.com>
2026-03-01 15:20:59 +0600

    feat(api): import Inventory schema in CatalogModule

--- apps/api/src/modules/catalog/catalog.module.ts
+++ apps/api/src/modules/catalog/catalog.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CaslModule } from "../casl/casl.module";
+import { Inventory } from "../inventory/schemas/inventory.schema";
 import { CatalogVendorController } from "./catalog-vendor.controller";
 import { CatalogController } from "./catalog.controller";
 import { CatalogService } from "./catalog.service";
 import { Product } from "./schemas/product.schema";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), CaslModule],
+  imports: [TypeOrmModule.forFeature([Product, Inventory]), CaslModule],
   controllers: [CatalogController, CatalogVendorController],
   providers: [CatalogService],
   exports: [CatalogService],
