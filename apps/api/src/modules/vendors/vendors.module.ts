a9af24384d4fda495ab0bc8bc3a994bf831d19ea
ujjal <ujjalroy7862@gmail.com>
2026-03-01 15:37:36 +0600

    feat(api): add VendorsModule

---
+++ apps/api/src/modules/vendors/vendors.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Product } from "../catalog/schemas/product.schema";
+import { CaslModule } from "../casl/casl.module";
+import { Inventory } from "../inventory/schemas/inventory.schema";
+import { Order } from "../orders/schemas/order.schema";
+import { Membership } from "./schemas/membership.schema";
+import { Vendor } from "./schemas/vendor.schema";
+import { VendorsAdminController } from "./vendors-admin.controller";
+import { VendorsController } from "./vendors.controller";
+import { VendorsService } from "./vendors.service";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Vendor, Membership, Product, Order, Inventory]),
+    CaslModule,
+  ],
+  controllers: [VendorsController, VendorsAdminController],
+  providers: [VendorsService],
+  exports: [VendorsService],
+})
+export class VendorsModule {}
