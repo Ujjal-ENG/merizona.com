d179ddf19471755c21a9d3a5914334dca74a1553
ujjal <ujjalroy7862@gmail.com>
2026-03-01 15:36:39 +0600

    refactor(api): import Vendor entity in CatalogModule

--- apps/api/src/modules/catalog/catalog.module.ts
+++ apps/api/src/modules/catalog/catalog.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CaslModule } from "../casl/casl.module";
 import { Inventory } from "../inventory/schemas/inventory.schema";
+import { Vendor } from "../vendors/schemas/vendor.schema";
 import { CatalogVendorController } from "./catalog-vendor.controller";
 import { CatalogController } from "./catalog.controller";
 import { CatalogService } from "./catalog.service";
 import { Product } from "./schemas/product.schema";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Inventory]), CaslModule],
+  imports: [TypeOrmModule.forFeature([Product, Inventory, Vendor]), CaslModule],
   controllers: [CatalogController, CatalogVendorController],
   providers: [CatalogService],
   exports: [CatalogService],
