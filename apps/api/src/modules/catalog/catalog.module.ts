db3604e384075fd3bad008871cee3fdbe6f6dd89
ujjal <ujjalroy7862@gmail.com>
2026-03-11 23:03:08 +0600

    feat(api): register product variant and image entities in CatalogModule

--- apps/api/src/modules/catalog/catalog.module.ts
+++ apps/api/src/modules/catalog/catalog.module.ts
@@ -6,10 +6,12 @@ import { Vendor } from "../vendors/schemas/vendor.schema";
 import { CatalogVendorController } from "./catalog-vendor.controller";
 import { CatalogController } from "./catalog.controller";
 import { CatalogService } from "./catalog.service";
+import { ProductImage } from "./schemas/product-image.schema";
+import { ProductVariant } from "./schemas/product-variant.schema";
 import { Product } from "./schemas/product.schema";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Inventory, Vendor]), CaslModule],
+  imports: [TypeOrmModule.forFeature([Product, ProductVariant, ProductImage, Inventory, Vendor]), CaslModule],
   controllers: [CatalogController, CatalogVendorController],
   providers: [CatalogService],
   exports: [CatalogService],
